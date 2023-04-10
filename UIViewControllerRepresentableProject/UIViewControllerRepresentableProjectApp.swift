67ee8c8af54ba781dbd19cd436f4cc3b3cbbd210
Divino Borges <divino.borges@mindsdigital.net>
2023-04-10 13:58:59 -0300

    Initial Commit

---
+++ UIViewControllerRepresentableProject/UIViewControllerRepresentableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableProjectApp.swift
+//  UIViewControllerRepresentableProject
+//
+//  Created by Divino Borges on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
